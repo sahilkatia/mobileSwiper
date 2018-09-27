5a8f94675d2f6fc58c1820aa851a0a6af174b628
=Sahil <=sahil.katia7@gmail.com>
2018-09-26 21:59:11 -0400

    swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component , ViewEncapsulation} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SwiperComponent, SwiperDirective, SwiperConfigInterface,SwiperScrollbarInterface } from 'ngx-swiper-wrapper';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'swiper';
+  public slides = [1, 2, 3,4,5,6].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
+  public config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: true,
+    pagination: false
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SwiperModule, SwiperConfigInterface,SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
